372faa6f6201ed8d496bf0e86fafea2d14fd09fb
LucasGdosR <lucas.gribel.reis@gmail.com>
2025-11-24 11:27:25 -0300

    type

--- src/main.c
+++ src/main.c
@@ -2,6 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
+char *builtins[3] = {"echo\n", "type\n", "exit\n"};
+
+void handle_type(char *line, int len) {
+  if (len == 5) {
+    for (int i = 0; i < sizeof(builtins) / sizeof(builtins[0]); ++i)
+    {
+      if (strcmp(line, builtins[i]) == 0) {
+        line[len - 1] = '\0';
+        printf("%s is a shell builtin\n", line);
+        return;
+      }
+    }
+  }
+  printf("%s: not found\n", line);
+}
+
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
@@ -15,6 +31,8 @@ int main(int argc, char *argv[]) {
     if (strcmp(command, "exit\n") == 0) exit(0);
     else if (len >= 5 && strncmp(command, "echo ", 5) == 0) {
       printf("%s", command + 5);
+    } else if (len >= 5 && strncmp(command, "type ", 5) == 0) {
+      handle_type(command + 5, len - 5);
     } else {
       command[len - 1] = '\0';
       printf("%s: command not found\n", command);
