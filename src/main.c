c9ef8d91d875e7e10bd23558e0b5c7d0ef52d236
LucasGdosR <lucas.gribel.reis@gmail.com>
2025-11-24 11:07:50 -0300

    echo

--- src/main.c
+++ src/main.c
@@ -11,12 +11,14 @@ int main(int argc, char *argv[]) {
   {
     printf("$ ");
     fgets(command, 1024, stdin);
-    if (strcmp(command, "exit\n"))
-    {
-      command[strlen(command) - 1] = '\0';
+    int len = strlen(command);
+    if (strcmp(command, "exit\n") == 0) exit(0);
+    else if (len >= 5 && strncmp(command, "echo ", 5) == 0) {
+      printf("%s", command + 5);
+    } else {
+      command[len - 1] = '\0';
       printf("%s: command not found\n", command);
     }
-    else exit(0);
   }
   return 0;
 }
