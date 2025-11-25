71b09e384e00699cf0070ba69592b9f27200df6f
LucasGdosR <lucas.gribel.reis@gmail.com>
2025-11-25 07:50:31 -0300

    refactor

--- src/main.c
+++ src/main.c
@@ -1,9 +1,14 @@
 #include <alloca.h>
 #include <dirent.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
+/*=================================================================================================
+  DEFINES
+=================================================================================================*/
 
 #ifdef _WIN32
 #define PATH_LIST_SEPARATOR ";"
@@ -11,22 +16,49 @@
 #define PATH_LIST_SEPARATOR ":"
 #endif
 
+/*=================================================================================================
+  STRUCTS
+=================================================================================================*/
+
 typedef struct str {
   char *data;
   unsigned long len;
 } str;
 
-str PATH;
-char *builtins[3] = {"echo", "type", "exit"};
+/*=================================================================================================
+  ENUMS
+=================================================================================================*/
 
-void handle_type(str *string) {
+enum Builtins {
+  Echo,
+  Type,
+  Exit,
+  Builtins_Size,
+};
+
+/*=================================================================================================
+  GLOBALS
+=================================================================================================*/
+
+str PATH;
+char *builtins[] = {[Echo]="echo", [Type]="type", [Exit]="exit"};
+
+/*=================================================================================================
+  FUNCTIONS
+=================================================================================================*/
+
+void handle_type(str *string)
+{
   string->data[--string->len] = '\0';
 
   // Builtin case:
-  if (string->len == 4) {
-    for (int i = 0; i < sizeof(builtins) / sizeof(builtins[0]); ++i)
+  // For now, all builtins coincidentally have length 4.
+  if (string->len == 4)
+  {
+    for (int i = 0; i < Builtins_Size; ++i)
     {
-      if (strcmp(string->data, builtins[i]) == 0) {
+      if (strcmp(string->data, builtins[i]) == 0)
+      {
         printf("%s is a shell builtin\n", string->data);
         return;
       }
@@ -35,21 +67,26 @@ void handle_type(str *string) {
 
   // Executable case:
   {
+    // PATH is immutable, so make a mutable copy.
     char *path = alloca(PATH.len);
-    strcpy(path, PATH.data);
+    memcpy(path, PATH.data, PATH.len+1);
+
     char *dir_str;
     while ((dir_str = strsep(&path, PATH_LIST_SEPARATOR)))
     {
       DIR *dir = opendir(dir_str);
       struct dirent *entry;
-      if (dir) while ((entry = readdir(dir))) {
-        if (entry->d_type == DT_REG && strcmp(entry->d_name, string->data) == 0) {
+      if (dir) while ((entry = readdir(dir)))
+      {
+        if (entry->d_type == DT_REG && strcmp(entry->d_name, string->data) == 0)
+        {
           unsigned long path_len = strlen(dir_str);
           char *full_path = alloca(string->len + path_len);
-          strcpy(full_path, dir_str);
+          memcpy(full_path, dir_str, path_len);
           full_path[path_len] = '/';
-          strcpy(full_path + path_len + 1, string->data);
-          if (access(full_path, X_OK) == 0) {
+          memcpy(full_path + path_len + 1, string->data, string->len + 1);
+          if (access(full_path, X_OK) == 0)
+          {
             printf("%s is %s\n", string->data, full_path);
             return;
           }
@@ -63,26 +100,36 @@ void handle_type(str *string) {
   printf("%s: not found\n", string->data);
 }
 
-int main(int argc, char *argv[]) {
+
+int main(int argc, char *argv[])
+{
   setbuf(stdout, NULL);
-  char command[1024];
+  char *buffer;
   PATH.data = getenv("PATH");
   PATH.len = strlen(PATH.data);
+
   for (;;)
   {
     printf("$ ");
-    fgets(command, 1024, stdin);
-    int len = strlen(command);
-    if (strcmp(command, "exit\n") == 0) exit(0);
-    else if (len >= 5 && strncmp(command, "echo ", 5) == 0) {
-      printf("%s", command + 5);
-    } else if (len >= 5 && strncmp(command, "type ", 5) == 0) {
-      str string = { command + 5, len - 5 };
+    size_t n = 0;
+    getline(&buffer, &n, stdin);
+    if (strcmp(buffer, "exit\n") == 0)
+      exit(0);
+    else if (n >= 5 && strncmp(buffer, "echo ", 5) == 0)
+    {
+      printf("%s", buffer + 5);
+    }
+    else if (n >= 5 && strncmp(buffer, "type ", 5) == 0)
+    {
+      str string = { buffer + 5, n - 5 };
       handle_type(&string);
-    } else {
-      command[len - 1] = '\0';
-      printf("%s: command not found\n", command);
+    }
+    else
+    {
+      buffer[n - 1] = '\0';
+      printf("%s: command not found\n", buffer);
     }
   }
+
   return 0;
 }
