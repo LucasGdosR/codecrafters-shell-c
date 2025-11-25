72c256275dcb1dc6e5d950f6caed34dcf407ef6c
LucasGdosR <lucas.gribel.reis@gmail.com>
2025-11-25 11:13:27 -0300

    pwd

--- src/main.c
+++ src/main.c
@@ -1,4 +1,3 @@
-#include <alloca.h>
 #include <assert.h>
 #include <dirent.h>
 #include <stddef.h>
@@ -17,6 +16,8 @@
 #else
 #define PATH_LIST_SEPARATOR ":"
 #endif
+#define ARENA_DEFAULT_SIZE 8192
+#define MAX_CWD_SIZE 1024
 
 /*=================================================================================================
   STRUCTS
@@ -27,11 +28,18 @@ typedef struct str {
   size_t len;
 } str;
 
+typedef struct arena {
+  char *data;
+  size_t len;
+  size_t capacity;
+} arena;
+
 /*=================================================================================================
   ENUMS
 =================================================================================================*/
 
 enum Builtins {
+  PWD,
   Echo,
   Type,
   Exit,
@@ -43,19 +51,107 @@ enum Builtins {
 =================================================================================================*/
 
 str PATH;
-char *builtins[] = {[Echo]="echo", [Type]="type", [Exit]="exit"};
+char *builtins[] = {[PWD]="pwd", [Echo]="echo", [Type]="type", [Exit]="exit"};
 
 /*=================================================================================================
   FUNCTIONS
 =================================================================================================*/
 
-void handle_type(str *string)
+void arena_init(arena *arena, size_t size);
+char* arena_push_string(arena *arena, size_t size);
+void arena_reset(arena *arena);
+char* find_executable(str *target, arena *allocator);
+void type_builtin(str *string, arena *allocator);
+
+int main(int argc, char *argv[])
+{
+  setbuf(stdout, NULL);
+  str buffer = {0};
+  PATH.data = getenv("PATH");
+  PATH.len = strlen(PATH.data);
+  arena temp_allocator = {0};
+  arena_init(&temp_allocator, ARENA_DEFAULT_SIZE);
+
+  // REPL
+  for (;;)
+  {
+    arena_reset(&temp_allocator);
+    printf("$ ");
+
+    // Read:
+    size_t len = getline(&buffer.data, &buffer.len, stdin);
+    if (len == -1) // EOF
+      break;
+
+    // Eval-Print: builtins:
+    // pwd
+    if (len == 4 && strcmp(buffer.data, "pwd\n") == 0)
+    {
+      char *cwd = arena_push_string(&temp_allocator, MAX_CWD_SIZE);
+      getcwd(cwd, MAX_CWD_SIZE);
+      printf("%s\n", cwd);
+    }
+    // exit
+    else if (len == 5 && strcmp(buffer.data, "exit\n") == 0)
+      exit(0);
+    // echo
+    else if (len >= 5 && strncmp(buffer.data, "echo ", 5) == 0)
+    {
+      printf("%s", buffer.data + 5);
+    }
+    // type
+    else if (len >= 5 && strncmp(buffer.data, "type ", 5) == 0)
+    {
+      str string = { buffer.data + 5, len - 5 };
+      type_builtin(&string, &temp_allocator);
+    }
+    // executable
+    else
+    {
+      // Remove trailing '\n'
+      buffer.data[len - 1] = '\0';
+
+      // Keep pointer to the start of the buffer for future iterations
+      char *bp = buffer.data;
+
+      str executable = {strsep(&bp, " ")};
+      executable.len = strlen(executable.data);
+      char *full_path = find_executable(&executable, &temp_allocator);
+      if (full_path) {
+        if (fork() == 0)
+        {
+          // Child process: get args and execute.
+          int MAX_ARGS = 64;
+          char *args[MAX_ARGS];
+          char *arg;
+          args[0] = executable.data;
+          int i = 1;
+          while ((arg = strsep(&bp, " "))) {
+            // Guard against multiple spaces.
+            if (arg[0] != '\0')
+              args[i++] = arg;
+            assert(i < MAX_ARGS);
+          }
+          args[i] = NULL;
+          execv(full_path, args);
+        }
+        // Original process
+        else wait(NULL);
+      }
+      else printf("%s: command not found\n", buffer.data);
+    }
+  }
+
+  return 0;
+}
+
+void type_builtin(str *string, arena *allocator)
 {
   string->data[--string->len] = '\0';
 
   // Builtin case:
-  // For now, all builtins coincidentally have length 4.
-  if (string->len == 4)
+  // For now, all builtins have length <= 4.
+  if (string->len <= 4)
   {
     for (int i = 0; i < Builtins_Size; ++i)
     {
@@ -68,133 +164,69 @@ void handle_type(str *string)
   }
 
   // Executable case:
-  {
-    // PATH is immutable, so make a mutable copy.
-    char *path = alloca(PATH.len+1);
-    memcpy(path, PATH.data, PATH.len+1);
-
-    char *dir_str;
-    while ((dir_str = strsep(&path, PATH_LIST_SEPARATOR)))
-    {
-      DIR *dir = opendir(dir_str);
-      struct dirent *entry;
-      if (dir) while ((entry = readdir(dir)))
-      {
-        if (entry->d_type == DT_REG && strcmp(entry->d_name, string->data) == 0)
-        {
-          unsigned long path_len = strlen(dir_str);
-          char *full_path = alloca(string->len + path_len + 2);
-          memcpy(full_path, dir_str, path_len);
-          full_path[path_len] = '/';
-          memcpy(full_path + path_len + 1, string->data, string->len + 1);
-          if (access(full_path, X_OK) == 0)
-          {
-            printf("%s is %s\n", string->data, full_path);
-            return;
-          }
-        }
-      }
-      closedir(dir);
-    }
-  }
-
+  char *full_path = find_executable(string, allocator);
+  if (full_path)
+    printf("%s is %s\n", string->data, full_path);
   // Default case:
-  printf("%s: not found\n", string->data);
+  else printf("%s: not found\n", string->data);
 }
 
-
-int main(int argc, char *argv[])
+char* find_executable(str *target, arena *allocator)
 {
-  setbuf(stdout, NULL);
-  str buffer = {0};
-  PATH.data = getenv("PATH");
-  PATH.len = strlen(PATH.data);
+  // PATH is immutable, so make a mutable copy.
+  char *path = arena_push_string(allocator, PATH.len+1);
+  memcpy(path, PATH.data, PATH.len+1);
 
-  // REPL
-  for (;;)
+  char *dir_str;
+  while ((dir_str = strsep(&path, PATH_LIST_SEPARATOR)))
   {
-    printf("$ ");
-
-    // Read:
-    unsigned long len = getline(&buffer.data, &buffer.len, stdin);
-
-    // Eval-Print: builtins:
-    // exit
-    if (strcmp(buffer.data, "exit\n") == 0)
-      exit(0);
-    // echo
-    else if (len >= 5 && strncmp(buffer.data, "echo ", 5) == 0)
+    DIR *dir = opendir(dir_str);
+    struct dirent *entry;
+    if (dir) while ((entry = readdir(dir)))
     {
-      printf("%s", buffer.data + 5);
-    }
-    // type
-    else if (len >= 5 && strncmp(buffer.data, "type ", 5) == 0)
-    {
-      str string = { buffer.data + 5, len - 5 };
-      handle_type(&string);
-    }
-    // executables or not found
-    else
-    {
-      buffer.data[len - 1] = '\0';
-
-      int executable_found = 0;
-      char *path = alloca(PATH.len+1);
-      memcpy(path, PATH.data, PATH.len+1);
-      char *bp = buffer.data;
-      char *executable_name = strsep(&bp, " ");
-      unsigned long executable_name_len = strlen(executable_name);
-
-      char *dir_str;
-      while ((dir_str = strsep(&path, PATH_LIST_SEPARATOR)) && !executable_found)
+      if (entry->d_type == DT_REG && strcmp(entry->d_name, target->data) == 0)
       {
-        DIR *dir = opendir(dir_str);
-        struct dirent *entry;
-        if (dir) while ((entry = readdir(dir)))
+        unsigned long path_len = strlen(dir_str);
+        char *full_path = arena_push_string(allocator, target->len + path_len + 2);
+        memcpy(full_path, dir_str, path_len);
+        full_path[path_len] = '/';
+        memcpy(full_path + path_len + 1, target->data, target->len + 1);
+        if (access(full_path, X_OK) == 0)
         {
-          if (entry->d_type == DT_REG && strcmp(entry->d_name, executable_name) == 0)
-          {
-            unsigned long path_len = strlen(dir_str);
-            char *full_path = alloca(executable_name_len + path_len + 2);
-            memcpy(full_path, dir_str, path_len);
-            full_path[path_len] = '/';
-            memcpy(full_path + path_len + 1, executable_name, executable_name_len + 1);
-            if (access(full_path, X_OK) == 0)
-            {
-              int rc = fork();
-              if (rc == 0)
-              {
-                // Child process: get args and execute.
-                int MAX_ARGS = 64;
-                char *args[MAX_ARGS];
-                char *arg;
-                args[0] = executable_name;
-                int i = 1;
-                while ((arg = strsep(&bp, " "))) {
-                  if (arg[0] != '\0')
-                    args[i++] = arg;
-                  assert(i < MAX_ARGS);
-                }
-                args[i] = NULL;
-                execv(full_path, args);
-              }
-              // Original process
-              else
-              {
-                executable_found = 1;
-                wait(NULL);
-                break;
-              }
-            }
-          }
+          closedir(dir);
+          return full_path;
         }
-        closedir(dir);
       }
-
-      if (!executable_found)
-        printf("%s: command not found\n", buffer.data);
     }
+    closedir(dir);
   }
+  return NULL;
+}
 
-  return 0;
+void arena_init(arena *arena, size_t size)
+{
+  arena->data = malloc(size);
+  arena->capacity = size;
+  if (!arena->data)
+  {
+    fprintf(stderr, "Failed initializing arena with %lu bytes.\n", size);
+    exit(1);
+  }
+}
+
+void arena_reset(arena *arena)
+{
+  arena->len = 0;
+}
+
+char* arena_push_string(arena *arena, size_t size)
+{
+  if (arena->capacity < arena->len + size)
+  {
+    fprintf(stderr, "Arena overflowed.\n");
+    exit(1);
+  }
+  char* result = arena->data + arena->len;
+  arena->len += size;
+  return result;
 }
